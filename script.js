1dc975938e81ef46f69799788ef3739aab8dab0e
heitornobrega <nobheitor@gmail.com>
2022-04-26 15:28:40 -0300

    teste de validação de funcao implementado

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function createProductImageElement(imageSource) {
+function createProductImageElement(imageSource) { //
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
 
-function createCustomElement(element, className, innerText) {
+function createCustomElement(element, className, innerText) { //
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ sku, name, image }) { // cria uma section e add filhos a ela
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -25,14 +25,14 @@ function createProductItemElement({ sku, name, image }) {
 }
 
 function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+  return item.querySelector('span.item__sku').innerText;// pega o sku do item, que está no innertext da tag span
 }
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
 }
-
-function createCartItemElement({ sku, name, salePrice }) {
+// Cria item de lista contendo especificaçoes do produto, adiciona a essa lista uma classe 'cart-item' e um evento de click que dispara funcão  cartItemClickListner
+function createCartItemElement({ sku, name, salePrice }) { 
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
