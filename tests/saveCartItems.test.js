9b36eef80fee26be4284a4b95d80c8bde2446234
heitornobrega <nobheitor@gmail.com>
2022-04-27 03:40:56 -0300

    Teste saveCartItems 2 implementado

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -8,4 +8,8 @@ describe('4 - Teste a função saveCartItems', () => {
     saveCartItems('<ol><li>Item</li></ol>');
     expect(localStorage.setItem).toHaveBeenCalled();
   })
+  it('Testa se saveCartItems("<ol><li>Item</li></ol>") invoca o método localStorage.setItem com dois parâmetros, o primeiro "cartItems" e o segundo o valor passado', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  })
 });
